afba808d99d6bb3a4411be2fd6b27cbf4b16d259
MohseenAH <mohsin743hawaldar@gmail.com>
2026-04-13 00:26:35 +0530

    merged both frontend and backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config(); // ✅ MUST BE FIRST
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import sequelize from "./config/db.js";
 import "./models/index.js";
